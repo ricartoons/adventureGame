5c13702529f05abda051a1defda26defc3f84410
Ricardo Justino <ricartoons@hotmail.com>
2019-07-22 16:31:57 -0300

    [WIP]{Core} - adicao da descricao em algumas telas

--- core.js
+++ core.js
@@ -111,15 +111,15 @@ class Player {
 
 const map = [
   { id: 0, name: 'Woods', directions: [{name: 'left', idNextScenario: 1}, {name: 'right', idNextScenario: 2}], objects: ['branch'], description: 'You are a forest, not very dense, so you can see in the distance. In your left there is a WaterFall and in right a Rocky Mountains, what are you going to do?' },
-  { id: 1, name: 'WaterFall', directions: [{name: 'down', idNextScenario: 4}, {name: 'back', idNextScenario: 0}], objects: [], description: 'There is a waterfall right in front of you, you calculate that it is not very high, but it does not seem clever to dive from this height' },
+  { id: 1, name: 'WaterFall', directions: [{name: 'climb', idNextScenario: 4}, {name: 'back', idNextScenario: 0}], objects: [], description: 'There is a waterfall right in front of you, you calculate that it is not very high, but it does not seem clever to dive from that height. However, there are some creepers on the slope, maybe we can climb' },
   { id: 2, name: 'Rocky Mountains', directions: [{name: 'climb', idNextScenario: 3}, {name: 'back', idNextScenario: 0}], objects: ['naylon', 'rope'], description: 'A kind of rock wall, there are a lot of cracks in it, maybe you can climb' },
   { id: 3, name: 'Sky dive', directions: [{name: 'jump', idNextScenario: 4}, {name: 'back', idNextScenario: 2}], objects: [], description: 'Definitely this is a hang gliding runway, it is quite high. You can see a river below and a lake a few kilometers ahead.' },
   { id: 4, name: 'River', directions: [{name: 'boat', idNextScenario: 5}], objects: [], description: 'The current is very strong, it seems to be a deep river, the water is very dirty with earth.' },
   { id: 5, name: 'Lake', directions: [{name: 'island', idNextScenario: 6}], objects: [], description: 'Despite the river the lake is very calm, its surface is flatly flat, it looks like a mirror and it is difficult to differentiate where the sky ends and the lake begins' },
-  { id: 6, name: 'Island beach', directions: [{name: 'old woods', idNextScenario: 7}, {name: 'temple', idNextScenario: 8}, {name: 'cemetery', idNextScenario: 9}, {name: 'boat', idNextScenario: 5}], objects: ['lamp'], description: ''},
-  { id: 7, name: 'Old woods', directions: [{name: 'beach', idNextScenario: 6}, {name: 'temple', idNextScenario: 8},  {name: 'cemetery', idNextScenario: 9}, {name: 'back', idNextScenario: 6} ], objects: ['knife'], description: ''},
-  { id: 8, name: 'Temple', directions: [{name: 'old woods', idNextScenario: 7}, {name: 'enter temple', idNextScenario: 10}, {name: 'beach', idNextScenario: 8},  {name: 'cemetery', idNextScenario: 9}, {name: 'back', idNextScenario: 6} ], objects: [], description: ''},
-  { id: 9, name: 'Cemetery', directions: [{name: 'old woods', idNextScenario: 7}, {name: 'temple', idNextScenario: 8},  {name: 'beach', idNextScenario: 6}, {name: 'back', idNextScenario: 6} ], objects: ['shovel'], description: ''},
+  { id: 6, name: 'Island beach', directions: [{name: 'bamboo woods', idNextScenario: 7}, {name: 'temple', idNextScenario: 8}, {name: 'cemetery', idNextScenario: 9}, {name: 'boat', idNextScenario: 5}], objects: ['lamp'], description: 'Apparently there is nobody living on the island, we have a beach and a forest composed basically of bamboos'},
+  { id: 7, name: 'Bamboo woods', directions: [{name: 'beach', idNextScenario: 6}, {name: 'temple', idNextScenario: 8},  {name: 'cemetery', idNextScenario: 9}, {name: 'back', idNextScenario: 6} ], objects: ['knife'], description: ''},
+  { id: 8, name: 'Temple', directions: [{name: 'bamboo woods', idNextScenario: 7}, {name: 'enter temple', idNextScenario: 10}, {name: 'beach', idNextScenario: 8},  {name: 'cemetery', idNextScenario: 9}, {name: 'back', idNextScenario: 6} ], objects: [], description: ''},
+  { id: 9, name: 'Cemetery', directions: [{name: 'bamboo woods', idNextScenario: 7}, {name: 'temple', idNextScenario: 8},  {name: 'beach', idNextScenario: 6}, {name: 'back', idNextScenario: 6} ], objects: ['shovel'], description: ''},
   { id: 10, name: 'Inside Temple', directions: [{name: 'leave', idNextScenario: 8}, {name: 'door 1', idNextScenario: 11}, {name: 'door 2', idNextScenario: 12} ], objects: ['parchment'], description: ''},
   { id: 11, name: 'Cerimony room', directions: [{name: 'back to main room', idNextScenario: 10}, {name: 'move statue', idNextScenario: 11} ], objects: ['key'], description: ''},
   { id: 12, name: 'Closet', directions: [{name: 'back to main room', idNextScenario: 10}, {name: 'open chest', idNextScenario: 11} ], objects: ['amulet'], description: ''}
@@ -130,7 +130,6 @@ let atualLocalization = '';
 function start(){
   player = new Player('Ricardo');
   atualLocalization= map[0];
-  //let test = null;
 }
 
 start();
@@ -153,23 +152,41 @@ player.lookBackpack();
 
 
 player.goto('climb');
+player.look();
+player.search();
+
 
 player.goto('back');
-
 player.goto('back');
+
 
 player.goto('left');
+player.look();
+player.search();
 
-player.goto('back');
 
-player.goto('left');
+player.goto('climb');
+player.look();
 
-player.goto('down');
 
 player.goto('boat');
+player.look();
+
 
 player.goto('island');
+player.look();
 
-player.goto('temple');
 
-player.goto('enter temple');
+// player.goto('back');
+
+// player.goto('left');
+
+// 
+
+// 
+
+// player.goto('island');
+
+// player.goto('temple');
+
+// player.goto('enter temple');
